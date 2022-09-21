4b28636fe5dcd1ec6d7e03936015217f4afc843c
Carlos Bernal <ramirez.bernal.jesus1@gmail.com>
2022-09-20 20:44:55 -0700

    db deploy

--- config.py
+++ config.py
@@ -2,8 +2,12 @@ class DevelopmentConfig():
     DEBUG = True
     PORT = 5001
     MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_DB = 'loginRest'
+    # MYSQL_USER = 'root'
+    # MYSQL_DB = 'loginRest'
+    MYSQL_USER = 'b6c581180a5036'
+    MYSQL_PASSWORD = '6590ad23'
+    MYSQL_DB = 'heroku_2f678db4338be62'
+
 
 config = {
     'development': DevelopmentConfig
